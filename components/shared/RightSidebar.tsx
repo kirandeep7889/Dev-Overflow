8e189c4b50f7665e109c69cff2bcb577547ca287
kirandeep7889 <124796483+kirandeep7889@users.noreply.github.com>
2024-06-27 16:30:05 +0530

    implement edit profile functionality

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -36,7 +36,8 @@ const popularTags = [
   { _id: "5", name: "ExpressJS", totalQuestions: 4 },
 ];
 
-const RightSidebar = () => {
+const RightSidebar = async () => {
+  // const hot = await getHotQuestions();
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex  h-max w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
